64a759375fb1e82988049613ee92a9e9103d9029
AtaMLK <126109381+AtaMLK@users.noreply.github.com>
2026-08-18 23:34:25 +0300

    chore: allow local network development origin

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "pludtlsowczexcydyrzt.supabase.co",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pludtlsowczexcydyrzt.supabase.co",
+        pathname: "/**",
+      },
+    ],
+  },
+  allowedDevOrigins: ["192.168.1.103"],
+};
 
-  export default nextConfig;
+export default nextConfig;
